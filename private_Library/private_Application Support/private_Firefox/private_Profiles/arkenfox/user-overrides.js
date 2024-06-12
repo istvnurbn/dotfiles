2997655633fea62d729f239c2be406931111ce25
István Urbán <51445152+istvnurbn@users.noreply.github.com>
2024-06-12 21:28:20 +0200

    Added comments

--- private_Library/private_Application Support/private_Firefox/private_Profiles/arkenfox/user-overrides.js
+++ private_Library/private_Application Support/private_Firefox/private_Profiles/arkenfox/user-overrides.js
@@ -1,6 +1,9 @@
+/* Disable translations ***/
 user_pref("browser.translations.automaticallyPopup", false);
 user_pref("browser.translations.enable", false);
 user_pref("browser.translations.panelShown", false);
+
+/* Weed out Pocket ***/
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.pocket.refresh.emailButton.enabled", false);
 user_pref("extensions.pocket.refresh.hideRecentSaves.enabled", false);
@@ -10,4 +13,6 @@ user_pref("browser.urlbar.suggest.pocket", false);
 user_pref("browser.newtabpage.activity-stream.discoverystream.saveToPocketCard.enabled", false);
 user_pref("browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+
+/* Use a decent password manager ;) ***/
 user_pref("signon.rememberSignons", false);
