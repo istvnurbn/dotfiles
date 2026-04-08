a4305f77da6afcaface4d43f6eb30ac8ac56ce8e
István Urbán <51445152+istvnurbn@users.noreply.github.com>
2026-04-08 10:45:10 +0200

    fix: rearrange dotfiles

--- modules/steve/dots.nix
+++ modules/steve/dots.nix
@@ -4,14 +4,15 @@
     dotsLink = path:
       config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/modules/steve/dots/${path}";
   in {
-    home.file.".zshrc".source = dotsLink "dot-zshrc";
-    home.file.".gitconfig".source = dotsLink "dot-gitconfig";
     home.file.".abcde".source = dotsLink "dot-abcde";
+    home.file.".gitconfig".source = dotsLink "dot-gitconfig";
+    home.file.".zshrc".source = dotsLink "dot-zshrc";
 
-    home.file.".config/ghostty".source = dotsLink "dot-config/ghostty";
-    home.file.".config/zed".source = dotsLink "dot-config/zed";
-    home.file.".config/atuin".source = dotsLink "dot-config/atuin";
+    home.file.".config/atuin/config.toml".source = dotsLink "dot-config/atuin/config.toml";
+    home.file.".config/dtop/config.yaml".source = dotsLink "dot-config/dtop/config.yaml";
+    home.file.".config/ghostty/config".source = dotsLink "dot-config/ghostty/config";
     home.file.".config/mc/ini".source = dotsLink "dot-config/mc/ini";
     home.file.".local/share/mc/skins/catppuccin.ini".source = dotsLink "dot-local/share/mc/skins/catppuccin.ini";
+    home.file.".config/zed/settings.json".source = dotsLink "dot-config/zed/settings.json";
   };
 }
