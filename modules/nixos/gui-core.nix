4bf901f21570eacc9c5e598be1d1c28a9a246a03
István Urbán <51445152+istvnurbn@users.noreply.github.com>
2026-04-20 17:45:39 +0200

    added: password managers, ghostty

--- modules/nixos/gui-core.nix
+++ modules/nixos/gui-core.nix
@@ -2,6 +2,9 @@ let
   flake.modules.nixos.gui-core = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
       alacritty
+      ghostty
+      bitwarden-desktop
+      proton-pass
       gimp
       picard
       audacious
