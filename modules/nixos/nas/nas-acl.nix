da842b3ec8f8a3244cd99b585df363e665fd904d
István Urbán <51445152+istvnurbn@users.noreply.github.com>
2026-05-11 11:04:05 +0200

    acl oops

--- modules/nixos/nas/nas-acl.nix
+++ modules/nixos/nas/nas-acl.nix
@@ -3,14 +3,15 @@ let
     # ACL rules for common folders
     systemd.tmpfiles.rules = [
       # Docker container data
-      "d  /srv/docker                       2775  steve homelab - -"
-      "Z  /srv/docker                       2775  steve homelab - -"
-      "A  /srv/docker                       -     -     -       - user:steve:rwx"
-      "A  /srv/docker                       -     -     -       - default:user:steve:rwx"
-      "A  /srv/docker                       -     -     -       - group:homelab:rwx"
-      "A  /srv/docker                       -     -     -       - default:group:homelab:rwx"
-      "A  /srv/docker/qbittorrent/postgres  -     -     -       - user:qui-postgres:rwx"
-      "A  /srv/docker/qbittorrent/postgres  -     -     -       - default:user:qui-postgres:rwx"
+      "d  /srv/docker                       2775  steve         homelab - -"
+      "Z  /srv/docker                       2775  steve         homelab - -"
+      "A  /srv/docker                       -     -             -       - user:steve:rwx"
+      "A  /srv/docker                       -     -             -       - default:user:steve:rwx"
+      "A  /srv/docker                       -     -             -       - group:homelab:rwx"
+      "A  /srv/docker                       -     -             -       - default:group:homelab:rwx"
+      "Z  /srv/docker/qbittorrent/postgres  2775  qui-postgres  homelab - -"
+      "A  /srv/docker/qbittorrent/postgres  -     -             -       - user:qui-postgres:rwx"
+      "A  /srv/docker/qbittorrent/postgres  -     -             -       - default:user:qui-postgres:rwx"
 
       # Shared files
       "Z  /srv/data 2775  steve homelab - -"
