2aec050ae7969e02fc8149d34ba1a467788a761c
István Urbán <51445152+istvnurbn@users.noreply.github.com>
2026-05-11 10:58:41 +0200

    reworked acls

--- modules/nixos/nas/nas-acl.nix
+++ modules/nixos/nas/nas-acl.nix
@@ -2,24 +2,28 @@ let
   flake.modules.nixos.nas-acl = {
     # ACL rules for common folders
     systemd.tmpfiles.rules = [
-      "d  /srv/docker 2775  steve homelab - -"
-      "Z  /srv/docker 2775  steve homelab - -"
-      "a+ /srv/docker -     -     -       - user:steve:rwx"
-      "a+ /srv/docker -     -     -       - default:user:steve:rwx"
-      "a+ /srv/docker -     -     -       - group:homelab:rwx"
-      "a+ /srv/docker -     -     -       - default:group:homelab:rwx"
+      # Docker container data
+      "d  /srv/docker                       2775  steve homelab - -"
+      "Z  /srv/docker                       2775  steve homelab - -"
+      "A  /srv/docker                       -     -     -       - user:steve:rwx"
+      "A  /srv/docker                       -     -     -       - default:user:steve:rwx"
+      "A  /srv/docker                       -     -     -       - group:homelab:rwx"
+      "A  /srv/docker                       -     -     -       - default:group:homelab:rwx"
+      "A  /srv/docker/qbittorrent/postgres  -     -     -       - user:qui-postgres:rwx"
+      "A  /srv/docker/qbittorrent/postgres  -     -     -       - default:user:qui-postgres:rwx"
 
+      # Shared files
       "Z  /srv/data 2775  steve homelab - -"
-      "a+ /srv/data -     -     -       - user:steve:rwx"
-      "a+ /srv/data -     -     -       - default:user:steve:rwx"
-      "a+ /srv/data -     -     -       - group:homelab:rwx"
-      "a+ /srv/data -     -     -       - default:group:homelab:rwx"
+      "A  /srv/data -     -     -       - user:steve:rwx"
+      "A  /srv/data -     -     -       - default:user:steve:rwx"
+      "A  /srv/data -     -     -       - group:homelab:rwx"
+      "A  /srv/data -     -     -       - default:group:homelab:rwx"
 
       "Z  /srv/public 2775  steve homelab - -"
-      "a+ /srv/public -     -     -       - user:steve:rwx"
-      "a+ /srv/public -     -     -       - default:user:steve:rwx"
-      "a+ /srv/public -     -     -       - group:homelab:rwx"
-      "a+ /srv/public -     -     -       - default:group:homelab:rwx"
+      "A  /srv/public -     -     -       - user:steve:rwx"
+      "A  /srv/public -     -     -       - default:user:steve:rwx"
+      "A  /srv/public -     -     -       - group:homelab:rwx"
+      "A  /srv/public -     -     -       - default:group:homelab:rwx"
     ];
   };
 in {
