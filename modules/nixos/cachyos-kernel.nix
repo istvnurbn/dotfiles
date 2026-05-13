251060acfb2c177f450c2d935bdee03195641351
István Urbán <51445152+istvnurbn@users.noreply.github.com>
2026-05-13 13:38:44 +0200

    switched to pinned CachyOS kernel

--- modules/nixos/cachyos-kernel.nix
+++ modules/nixos/cachyos-kernel.nix
@@ -1,11 +1,11 @@
 {inputs, ...}: let
   flake-file.inputs = {
-    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel?rev=de4bf344b26f355b5d92071503f7a8fd18bc7f6b";
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel";
   };
 
   flake.modules.nixos.cachyos-kernel = {pkgs, ...}: {
     # Enabling CachyOS kernel overlay
-    nixpkgs.overlays = [inputs.nix-cachyos-kernel.overlays.default];
+    nixpkgs.overlays = [inputs.nix-cachyos-kernel.overlays.pinned];
 
     # Setting the kernel package
     boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-zen4;
